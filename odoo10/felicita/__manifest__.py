dfc8bebc941ec198fdfd373f769bcc49db303b25
Carlos R. Silveira <crsilveira@gmail.com>
2016-11-24 15:36:15 -0200

    Customização.

---
+++ odoo10/felicita/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Felicita',
+    'version': '1.0',
+    'category': 'Localisation',
+    'author': 'ATS Solucoes',
+    'website': 'http://www.atsti.com.br',
+    'license': 'AGPL-3',
+    'contributors': [
+        'Carlos Silveira<carlos@atsti.com.br>',
+    ],
+    'depends': [
+        'br_base',
+    ],
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+}
