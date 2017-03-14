b3da272068a4c530c0d54c759f1d9caad1698efd
Otávio Munhoz <otaviosilmunhoz@hotmail.com>
2017-03-14 16:51:22 -0300

    add sale_split
    Adicionado módulo sale_split

---
+++ sale_split/models/sale.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+
+
+from itertools import groupby
+from datetime import datetime, timedelta
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+from odoo.tools import float_is_zero, float_compare, DEFAULT_SERVER_DATETIME_FORMAT
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_invoice_create(self, grouped=False, final=False, type='service'):
+        inv_obj = self.env['account.invoice']
+        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+        invoices = {}
+        references = {}
+        for order in self:
+            group_key = order.id if grouped else (order.partner_invoice_id.id, order.currency_id.id)
+            for line in order.order_line.sorted(key=lambda l: l.qty_to_invoice < 0):
+                if line.product_id.fiscal_type == type:
+                    if float_is_zero(line.qty_to_invoice, precision_digits=precision):
+                        continue
+                    if group_key not in invoices:
+                        inv_data = order._prepare_invoice()
+                        invoice = inv_obj.create(inv_data)
+                        references[invoice] = order
+                        invoices[group_key] = invoice
+                    elif group_key in invoices:
+                        vals = {}
+                        if order.name not in invoices[group_key].origin.split(', '):
+                            vals['origin'] = invoices[group_key].origin + ', ' + order.name
+                        if order.client_order_ref and order.client_order_ref not in invoices[group_key].name.split(
+                                ', '):
+                            vals['name'] = invoices[group_key].name + ', ' + order.client_order_ref
+                        invoices[group_key].write(vals)
+                    if line.qty_to_invoice > 0:
+                        line.invoice_line_create(invoices[group_key].id, line.qty_to_invoice)
+                    elif line.qty_to_invoice < 0 and final:
+                        line.invoice_line_create(invoices[group_key].id, line.qty_to_invoice)
+
+
+
+            if references.get(invoices.get(group_key)):
+                if order not in references[invoices[group_key]]:
+                    references[invoice] = references[invoice] | order
+
+        if not invoices:
+            raise UserError(_('There is no invoicable line.'))
+
+        for invoice in invoices.values():
+            if not invoice.invoice_line_ids:
+                raise UserError(_('There is no invoicable line.'))
+            # If invoice is negative, do a refund invoice instead
+            if invoice.amount_untaxed < 0:
+                invoice.type = 'out_refund'
+                for line in invoice.invoice_line_ids:
+                    line.quantity = -line.quantity
+            # Use additional field helper function (for account extensions)
+            for line in invoice.invoice_line_ids:
+                line._set_additional_fields(invoice)
+            # Necessary to force computation of taxes. In account_invoice, they are triggered
+            # by onchanges, which are not triggered when doing a create.
+            invoice.compute_taxes()
+            invoice.message_post_with_view('mail.message_origin_link',
+                values={'self': invoice, 'origin': references[invoice]},
+                subtype_id=self.env.ref('mail.mt_note').id)
+        return [inv.id for inv in invoices.values()]
+
+
+class SaleAdvancePaymentInv(models.TransientModel):
+
+    _inherit = "sale.advance.payment.inv"
+
+    @api.multi
+    def create_invoices(self):
+        sale_orders = self.env['sale.order'].browse(self._context.get('active_ids', []))
+        present_fiscal_type = []
+        if self.advance_payment_method == 'delivered':
+            sale_orders.action_invoice_create()
+        elif self.advance_payment_method == 'all':
+            for line in sale_orders.order_line:
+                if len(present_fiscal_type)>=1:
+                    if not any(type==line.product_id.fiscal_type for type in present_fiscal_type):
+                        present_fiscal_type.append(line.product_id.fiscal_type)
+                else:
+                    present_fiscal_type.append(line.product_id.fiscal_type)
+            for fiscal_type in present_fiscal_type:
+                sale_orders.action_invoice_create(final=True, type=fiscal_type)
+        else:
+            # Create deposit product if necessary
+            if not self.product_id:
+                vals = self._prepare_deposit_product()
+                self.product_id = self.env['product.product'].create(vals)
+                self.env['ir.values'].sudo().set_default('sale.config.settings', 'deposit_product_id_setting',
+                                                         self.product_id.id)
+
+            sale_line_obj = self.env['sale.order.line']
+            for order in sale_orders:
+                if self.advance_payment_method == 'percentage':
+                    amount = order.amount_untaxed * self.amount / 100
+                else:
+                    amount = self.amount
+                if self.product_id.invoice_policy != 'order':
+                    raise UserError(_(
+                        'The product used to invoice a down payment should have an invoice policy set to "Ordered quantities". Please update your deposit product to be able to create a deposit invoice.'))
+                if self.product_id.type != 'service':
+                    raise UserError(_(
+                        "The product used to invoice a down payment should be of type 'Service'. Please use another product or update this product."))
+                if order.fiscal_position_id and self.product_id.taxes_id:
+                    tax_ids = order.fiscal_position_id.map_tax(self.product_id.taxes_id).ids
+                else:
+                    tax_ids = self.product_id.taxes_id.ids
+                so_line = sale_line_obj.create({
+                    'name': _('Advance: %s') % (time.strftime('%m %Y'),),
+                    'price_unit': amount,
+                    'product_uom_qty': 0.0,
+                    'order_id': order.id,
+                    'discount': 0.0,
+                    'product_uom': self.product_id.uom_id.id,
+                    'product_id': self.product_id.id,
+                    'tax_id': [(6, 0, tax_ids)],
+                })
+                self._create_invoice(order, so_line, amount)
+        if self._context.get('open_invoices', False):
+            return sale_orders.action_view_invoice()
+        return {'type': 'ir.actions.act_window_close'}
