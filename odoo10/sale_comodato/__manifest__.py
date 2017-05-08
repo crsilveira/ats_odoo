a805c4561d667c9c2a53fe7772b7278be9413d09
Carlos R. Silveira <crsilveira@gmail.com>
2017-05-08 10:08:49 -0300

    Opção de orçamento com comodato.

---
+++ odoo10/sale_comodato/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': ' Comodato',
+    'version': '1.0',
+    'category': 'Sale',
+    'description': """
+    """,
+    'author': 'ATS Solucoes',
+    'website': 'http://www.atsti.com.br',
+    'depends': ['br_sale','br_account'],
+    'data': [
+        'views/sale_view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
